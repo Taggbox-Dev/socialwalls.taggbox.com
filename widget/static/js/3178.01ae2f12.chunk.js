2b2268fc27c97240a5828d07b51d09804f66ba23
kamrantaggbox <kamran@taggbox.com>
2024-04-29 18:51:20 +0530

    update

---
+++ widget/static/js/3178.01ae2f12.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkembed_widget=self.webpackChunkembed_widget||[]).push([[3178],{13178:(e,l,a)=>{a.r(l),a.d(l,{default:()=>n});var s=a(72791),t=a(54270),i=a(80184);class n extends s.PureComponent{render(){const{appData:e}=this.props;return(0,i.jsx)(t.q,{children:(0,i.jsx)("title",{children:e.wall.isDisplay?e&&e.wall&&Object.keys(e.wall).length>0&&e.wall.Wall?"".concat(e.wall.Wall.name," | Live Socialwall - Taggbox"):"Display":"Widget"})})}}}}]);
+//# sourceMappingURL=3178.01ae2f12.chunk.js.map
